d1837a04db19ee9c59b65b8f812f6f3fc0e60393
a20202042 <a20202042@gmail.com>
2021-06-20 14:39:49 +0800

    t commit

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn --pythonpath djangoProject3 djangoProject3.wsgi
+web: gunicorn --pythonpath  djangoProject3 djangoProject3.wsgi --log-file -
--- djangoProject3/production_settings.py
+++ djangoProject3/production_settings.py
@@ -1,18 +1,10 @@
-# Import all default settings.
-from djangoProject3.settings import *
 import dj_database_url
+from .settings import * # 含入原本的settings.py所有設定
+# heroku使用的資料庫為PostgreSQL，所以要修改資料庫設定
 DATABASES = {
     'default': dj_database_url.config(),
 }
-
-# Static asset configuration.
-STATIC_ROOT = 'staticfiles'
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure().
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-# Allow all host headers.
-ALLOWED_HOSTS = ['*']
-
-# Turn off DEBUG mode.
-DEBUG = False
+STATIC_ROOT = 'staticfiles' # 設定網站正式上線時靜態檔案目錄位置
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https') # 設定HTTP連線方式
+ALLOWED_HOSTS = ['*'] # 讓所有的網域都能瀏覽本網站
+DEBUG = False # 關閉除錯模式
